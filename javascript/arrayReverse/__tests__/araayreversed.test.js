3ff10f5c6570c2b043fc94f12ff9e770698e908f
anwarshraideh <ahshraida@gmail.com>
2021-05-16 20:22:46 +0300

    fix

--- javascript/arrayReverse/__tests__/araayreversed.test.js
+++ javascript/arrayReverse/__tests__/araayreversed.test.js
@@ -3,8 +3,8 @@ const reverseArray = require('../arrayReverse.js');
 
 describe('Testing 401 challenge 1', () => {
   test('It should return a array of reversed value', () => {
-    expect(reverseArray([1, 2, 3, 4, 5, 6]).toStrictEqual([6, 5, 4, 3, 2, 1]));
-    expect(reverseArray([89, 2354, 3546, 23, 10, -923, 823, -12]).toStrictEqual([-12, 823, -923, 10, 23, 3546, 2354, 89]));
+    expect(reverseArray([1, 2, 3, 4, 5, 6])).toStrictEqual([6, 5, 4, 3, 2, 1]);
+    expect(reverseArray([89, 2354, 3546, 23, 10, -923, 823, -12])).toStrictEqual([-12, 823, -923, 10, 23, 3546, 2354, 89]);
 
   });
 });
