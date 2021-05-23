872f13f35a31db7436af34b7e1cdf12c646cc1bb
anwarshraideh <ahshraida@gmail.com>
2021-05-23 16:11:32 +0300

    append

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -43,6 +43,27 @@ class LinkedList {
     return listString;
   }
 
+  ////////////////// ll-insertion///////////////////////
+
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      // this.length++;
+    } else {
+      let lastNode = this.head;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = node;
+      // this.length++;
+    }
+  }
+
+  
+ 
+
+
 }
 
 module.exports = LinkedList;
