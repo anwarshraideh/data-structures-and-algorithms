d8600eb016713d9005001770221a961dcceb5e76
anwarshraideh <ahshraida@gmail.com>
2021-05-23 15:45:52 +0300

    dd

--- javascript/ll-insertions/__tests__/linked-list.test.js
+++ javascript/ll-insertions/__tests__/linked-list.test.js
@@ -3,39 +3,49 @@
 // Require our linked list implementation
 const LinkedList = require('../linked-list');
 let list = new LinkedList();
-let newList = new LinkedList();
+
 
 describe('LL Insertions', () => {
-  it('should append new node to empty linked list', () => {
-    newList.append(83);
-    expect(newList.head.value).toBe(83);
-    expect(newList.head.next).toBeNull();
+  it('append a value to the end of linked list', () => {
+    let valueOne = new Node(1);
+    let valueTwo = new Node(2);
+    let valueThree = new Node(3);
+
+    list.insert(valueOne);
+    list.insert(valueTwo);
+    list.insert(valueThree);
+
+    list.append(4);
+    expect(list.toString()).toEqual('{3} ->{2} ->{1} ->{4} ->NULL');
+
   });
-  it('should append new node to the end of existing linked list', () => {
-    newList.append(61);
-    expect(newList.head.value).toBe(83);
-    expect(newList.head.next.value).toBe(61);
-    expect(newList.head.next.next).toBeNull();
+  it('should append multible value to the end of linked list', () => {
+    let valueOne = new Node(1);
+    let valueTwo = new Node(2);
+    let valueThree = new Node(3);
+
+    list.insert(valueOne);
+    list.insert(valueTwo);
+    list.insert(valueThree);
+
+    list.append(4);
+    list.append(5);
+    expect(list.toString()).toEqual('{3} ->{2} ->{1} ->{4} ->{5} ->NULL');
+
+
   });
   it('should insert a value before the given value in the linked list', () => {
-    expect(list.toString()).toBe('{ 57 } -> { 5 } -> { 13 } -> NULL');
-    list.insertBefore(57, 91);
-    expect(list.toString()).toBe('{ 91 } -> { 57 } -> { 5 } -> { 13 } -> NULL');
-    list.insertBefore(5, 23);
-    expect(list.toString()).toBe('{ 91 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> NULL');
-    list.insertBefore(3, 20);
-    expect(list.toString()).toBe('{ 91 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> NULL');
-    expect(list.insertBefore(3, 20)).toBe('Value doesn\'t exist');
+
   });
   it('should insert a value after the given value in the linked list', () => {
-    expect(list.toString()).toBe('{ 91 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> NULL');
-    list.insertAfter(91, 40);
-    expect(list.toString()).toBe('{ 91 } -> { 40 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> NULL');
-    list.insertAfter(13, 76);
-    expect(list.toString()).toBe('{ 91 } -> { 40 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> { 76 } -> NULL');
-    list.insertAfter(20, 76);
-    expect(list.insertBefore(20, 76)).toBe('Value doesn\'t exist');
-    expect(list.toString()).toBe('{ 91 } -> { 40 } -> { 57 } -> { 23 } -> { 5 } -> { 13 } -> { 76 } -> NULL');
+
+  });
+
+  it('should insert a value before the given value in the linked list', () => {
+
+  });
+  it('should insert a value after the given value in the linked list', () => {
+
   });
 });
 
--- javascript/ll-insertions/linked-list.js
+++ javascript/ll-insertions/linked-list.js
@@ -7,60 +7,22 @@ class LinkedList {
     this.head = null;
   }
 
-  append(value){
-    var newNode = new Node(value);
-    if(!this.head){
-      this.head = newNode;
+
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    //   this.length++;
     } else {
       let lastNode = this.head;
-      while(lastNode.next) lastNode = lastNode.next;
-      lastNode.next = newNode;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = node;
+    //   this.length++;
     }
   }
 
-  insertBefore(value, newVal){
-    var newNode = new Node(newVal);
-    if (this.head) {
-      // to check if there is a list
-      let currentNode = this.head;
-      if (currentNode.value === value) {// check the head
-        newNode.next = currentNode;
-        this.head = newNode;
-        return;
-      }
-      while (currentNode.next) {
-        // check the nodes one by one
-        if (currentNode.next.value === value) {
-          let temp = currentNode.next;
-          currentNode.next = newNode;
-          newNode.next = temp;
-          // newNode.next = currentNode;
-          // currentNode = newNode;
-          // console.log(currentNode.value, currentNode.next.value, currentNode.next.next.value);
-          return currentNode.value;
-        }
-        currentNode = currentNode.next;
-      }
-    }
-    return 'Value doesn\'t exist';
-  }
-
-  insertAfter(value, newVal){
-    let newNode = new Node(newVal);
-    if (this.head) {
-      let current = this.head;
-      while (current) {
-        if (current.value === value) {
-          let temp = current.next;
-          current.next = newNode;
-          newNode.next = temp;
-          return;
-        }
-        current = current.next;
-      }
-    }
-    return 'Value doesn\'t exist';
-  }
 
 
 }
