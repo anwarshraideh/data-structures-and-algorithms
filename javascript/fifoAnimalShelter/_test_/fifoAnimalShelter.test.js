baab2036dc4679ffc0fb25f8d62a2edcf17a0d0a
anwarshraideh <ahshraida@gmail.com>
2021-05-31 16:44:54 +0300

    second

--- javascript/fifoAnimalShelter/_test_/fifoAnimalShelter.test.js
+++ javascript/fifoAnimalShelter/_test_/fifoAnimalShelter.test.js
@@ -4,18 +4,34 @@ const AnimalShelter = require('../fifoAnimalShelter.js');
 
 describe ('test',()=>{
 
-  it('test enqueue',()=>{
+  it('test enqueue a cat',()=>{
 
     let animalShelter = new AnimalShelter();
-
-    animalShelter.enqueue('cat');
-    expect(animalShelter.cat.front.value).toEqual('cat');
-
+    let animal = 'cat';
+    animalShelter.enqueue(animal);
+    expect(animalShelter.cat.front.value).toEqual(animal);
 
 
   });
 
+  it('test enqueue a dog',()=>{
 
+    let animalShelter = new AnimalShelter();
+    let animal = 'dog';
+    animalShelter.enqueue(animal );
+    expect(animalShelter.dog.front.value).toEqual(animal);
+
+
+  });
+
+  it('test enqueue others animal',()=>{
+
+    let animalShelter = new AnimalShelter();
+    let animal = 'duck';
+    animalShelter.enqueue(animal );
+    expect(animalShelter.enqueue(animal)).toEqual('This shelter can not shelters other animals');
+
+  });
 
 
 });
