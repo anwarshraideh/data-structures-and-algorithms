188e129411be05b5f6c01733e8e685d8f03b0050
anwarshraideh <ahshraida@gmail.com>
2021-06-01 22:36:44 +0300

    donee

--- javascript/multiBracketValidation/_test_/multiBracketValidation.test.js
+++ javascript/multiBracketValidation/_test_/multiBracketValidation.test.js
@@ -5,39 +5,11 @@ const multiBracketValidation = require('../multiBracketValidation.js');
 describe ('test multi Bracket Validation',() =>{
 
 
-  // it ('return false if there is only one bracket unclosed',() =>{
-
-
-  //   expect(multiBracketValidation('()[]]')).toBeFalsy();
-
-  // });
-
-
-  // it('return true if the brackets are completed', () => {
-  //   expect(multiBracketValidation('()[]{}')).toBeTruthy();
-
-
-  // });
-
-  // it ('return false if there is only one bracket unclosed',() =>{
-
-
-  //   expect(multiBracketValidation('(){}')).toBeTruthy();
-
-  // });
-
-  // it('return true if the brackets are completed', () => {
-  //   expect(multiBracketValidation('[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]')).toBeTruthy();
-
-
-  // });
-
   it('return false if  it only opend brackets  ', () => {
 
     expect(multiBracketValidation('({((')).toBeFalsy();
 
   });
-
 
 
   it('return false if there is a bracket not complete ', () => {
--- javascript/multiBracketValidation/multiBracketValidation.js
+++ javascript/multiBracketValidation/multiBracketValidation.js
@@ -1,61 +1,7 @@
-// 'use strict';
-
-// const { Stack } = require('../stacksAndQueues/stacks-and-queues.js');
-
-
-// let stack = new Stack();
-
-// function multiBracketValidation(input){
-
-//   let Input = input.split('');
-//   let openBracket;
-
-//   for (let i = 0 ; i < Input.length ; i++) {
-
-//     if ( Input[i] ==='[' || Input[i] ==='{' || Input[i] ==='(' ){
-
-//       stack.push(Input[i]);
-
-
-//     }
-
-//     else if (Input[i] ===']' || Input[i] ==='}' || Input[i] ===')' )
-
-//     {
-
-//       openBracket = stack.peek() + Input[i];
-
-//       if ( openBracket === '{}' || openBracket === '[]' || openBracket === '()' )
-//       {
-//         stack.pop();
-
-//       }
-//       else
-//       {
-//         return false;
-//       }
-
-//     }
-
-//   }
-
-//   return true ;
-
-
-
-
-// }
-
-
-// module.exports = multiBracketValidation;
-
-
-
 'use strict';
 
-// const { Stack } = require('../stacksAndQueues/stacks-and-queues.js');
 
-let multiBracketValidation = function (str) {
+let multiBracketValidation = function (input) {
   let stack = [];
   let map = {
     '(': ')',
@@ -63,22 +9,18 @@ let multiBracketValidation = function (str) {
     '{': '}'
   };
 
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < input.length; i++) {
 
-    // If character is an opening brace add it to a stack
-    if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
-      stack.push(str[i]);
+    if (input[i] === '(' || input[i] === '{' || input[i] === '[' ) {
+      stack.push(input[i]);
     }
-    //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
     else {
       let last = stack.pop();
 
-      //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
-      if (str[i] !== map[last]) {return false;}
+      if (input[i] !== map[last]) {return false;}
     }
   }
 
-  // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
   if (stack.length !== 0) {return false;}
 
   return true;
