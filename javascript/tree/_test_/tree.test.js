aac1571ba53fbbf42c46e459cd3b9c8f79038844
anwarshraideh <ahshraida@gmail.com>
2021-06-03 16:37:09 +0300

    dd

--- javascript/tree/_test_/tree.test.js
+++ javascript/tree/_test_/tree.test.js
@@ -1,14 +1,36 @@
 'use strict';
 
 const Node = require('../node.js');
-const { BinarySearchTree } =require('../tree.js');
+const { BinaryTree , BinarySearchTree } = require('../tree.js');
 
 describe(' Binary Search Tree ' , () =>{
 
+  let tree;
+  beforeAll(() => {
+    const one = new Node(1);
+    const two = new Node(2);
+    const three = new Node(3);
+    const four = new Node(4);
+    const five = new Node(5);
+    const six = new Node(6);
+    const seven = new Node(7);
+    const eight = new Node(8);
+    one.left = two;
+    one.right = three;
+    two.left = four;
+    two.right = five;
+    five.left = eight;
+    three.left = six;
+    three.right = seven;
+    tree = new BinaryTree(one);
+
+  });
+
+
   it('Can successfully instantiate an empty tree',()=>{
 
-    let tree = new BinarySearchTree();
-    expect(tree.root).toBeNull();
+    let binaryTree = new BinarySearchTree();
+    expect(binaryTree.root).toBeNull();
 
   });
 
@@ -39,17 +61,23 @@ describe(' Binary Search Tree ' , () =>{
 
   it('Can successfully return a collection from a preorder traversal',()=>{
 
-    let value = 2 ;
-    let root = new Node(value);
-    let binaryTree = new BinarySearchTree(root);
+    // let value = 2 ;
+    // let root = new Node(value);
+    // let binaryTree = new BinarySearchTree(root);
 
-    binaryTree.add(4);
-    binaryTree.add(11);
-    binaryTree.add(6);
-    binaryTree.add(9);
-    binaryTree.add(0);
+    // binaryTree.add(4);
+    // binaryTree.add(11);
+    // binaryTree.add(6);
+    // binaryTree.add(9);
+    // binaryTree.add(0);
 
-    expect(binaryTree.preOrder()).toStrictEqual([2, 0, 4, 11, 6, 9]);
+    // expect(binaryTree.preOrder()).toStrictEqual([2, 0, 4, 11, 6, 9]);
+
+
+    const expected = [1, 2, 4, 5, 8, 3, 6, 7];
+    const preOrder = tree.preOrder();
+    expect(preOrder).toEqual(expected);
+
 
   });
 
@@ -57,4 +85,3 @@ describe(' Binary Search Tree ' , () =>{
 
 
 });
-
