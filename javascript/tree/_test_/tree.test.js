566221241efcab1b409e266ec6789e499c4020fe
anwarshraideh <ahshraida@gmail.com>
2021-06-03 15:33:14 +0300

    vv

--- javascript/tree/_test_/tree.test.js
+++ javascript/tree/_test_/tree.test.js
@@ -24,16 +24,25 @@ describe(' Binary Search Tree ' , () =>{
 
   it('Can successfully add a left child and right child to a single root node',()=>{
 
-    let value = 20 ;
+    // let value = 20 ;
+    // let root = new Node(value);
+    // let binaryTree = new BinarySearchTree(root);
+
+    // binaryTree.add(30);
+    // binaryTree.add(10);
+
+    // expect(binaryTree.root.left.value).toEqual(10);
+    // expect(binaryTree.root.right.value).toEqual(11);
+
+    let value = 8 ;
     let root = new Node(value);
-    let binaryTree = new BinarySearchTree(root);
-
-    binaryTree.add(30);
-    binaryTree.add(10);
-
-    expect(binaryTree.root.left.value).toEqual(10);
-    expect(binaryTree.root.right.value).toEqual(11);
-
+    let tree = new BinarySearchTree(root);
+    const left = 3;
+    const right = 12;
+    tree.add(left);
+    tree.add(right);
+    expect(tree.root.left.value).toEqual(left);
+    expect(tree.root.right.value).toEqual(right);
 
   });
 
