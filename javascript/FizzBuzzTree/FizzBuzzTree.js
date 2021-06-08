cc0102dccffe2058017e8cdd7f910b0fc3e0c2f1
anwarshraideh <ahshraida@gmail.com>
2021-06-08 14:50:38 +0300

    cc

--- javascript/FizzBuzzTree/FizzBuzzTree.js
+++ javascript/FizzBuzzTree/FizzBuzzTree.js
@@ -1,40 +1,3 @@
-// 'use strict';
-
-// function FizzBuzzTree(tree) {
-//   let _traverse = (node) => {
-//     if (node.value) {
-//       if (node.value % 3 === 0 && node.value % 5 === 0){
-//         node.value = 'FizzBuzz';
-
-//       }else if (node.value % 5 === 0){
-//         node.value = 'Buzz';
-
-//       }else if (node.value % 3 === 0){
-//         node.value = 'Fizz';
-
-//       }else{
-//         //turn the number into a String
-//         node.value = `${node.value}`;
-//       }
-//     }
-//     if (node.left){
-//       _traverse(node.left);
-//     }
-//     if (node.right){
-//       _traverse(node.right);
-//     }
-//   };
-
-//   _traverse(tree.root);
-//   return tree;
-// }
-
-
-
-// module.exports = FizzBuzzTree;
-
-
-
 'use strict';
 
 class Node {
@@ -44,7 +7,7 @@ class Node {
   }
 }
 
-class KAryTree {
+class KaryTree {
   constructor(root = null) {
     this.root = root;
   }
@@ -53,18 +16,19 @@ class KAryTree {
     this.root = tree.root;
 
     const _traverse = (node) => {
-      if (!(node.value % 15)) {
-        node.value = 'FizzBuzz';
-      } else if (!(node.value % 5)) {
-        node.value = 'Buzz';
-      } else if (!(node.value % 3)) {
-        node.value = 'Fizz';
-      } else {
+      // if (!(node.value % 15)) !(node.value % 5) !(node.value % 3)
+      if (node.value % 15 === 0) { node.value = 'FizzBuzz';}
+      else if (node.value % 5 === 0) { node.value = 'Buzz';}
+      else if (node.value % 3 === 0) { node.value = 'Fizz';}
+
+      else {
         node.value = 'Not multiple of 3 nor 5';
       }
+
+
       if (node.children.length) {
-        for (let counter = 0; counter < node.children.length; counter++) {
-          _traverse(node.children[counter]);
+        for (let index = 0; index < node.children.length; index++) {
+          _traverse(node.children[index]);
         }
       }
     };
@@ -72,4 +36,4 @@ class KAryTree {
   }
 }
 
-module.exports = { Node, KAryTree };
+module.exports = { Node, KaryTree };
