f13d96ae9db7ad24294b13cbcb3a86cf91e945fd
anwarshraideh <ahshraida@gmail.com>
2021-06-24 16:37:20 +0300

    third

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,7 +1,41 @@
 'use strict';
 
-const LinkedList = require('../javascript/linked-list/linked-list.js');
+// const LinkedList = require('../javascript/linked-list/linked-list.js');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  values() {
+    let values = [];
+    let current = this.head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
+  }
+}
 class HashTable {
   constructor(length) {
     this.table = new Array(length);
