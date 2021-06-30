298aa807736bf4ed057b7d319f757e851a355957
anwarshraideh <ahshraida@gmail.com>
2021-06-30 13:41:56 +0300

    cc5

--- javascript/code-challenges/hashmap-left-join/hashmapleftjoin.js
+++ javascript/code-challenges/hashmap-left-join/hashmapleftjoin.js
@@ -1,33 +1,46 @@
 'use strict';
 
-// const {HashTable} = require('../hashTable/hash-table'); 
+// const {HashTable} = require('../hashTable/hash-table');
 
 function leftJoin(hashTable1, hashTable2) {
-  const leftJoinObj = {};
+  const leftJoinResult = [];
 
   hashTable1.table.forEach(bucket => {
     if (bucket) {
-      // console.log(bucket.root.value);
-      let key = Object.keys(bucket.root.value)[0];
-      leftJoinObj[key] = [bucket.root.value[key]];
+      let currentNode = bucket.head;
 
-    }
-  });
+      while (currentNode) {
 
-  hashTable2.table.forEach(bucket => {
-    if (bucket) {
-      let key = Object.keys(bucket.root.value)[0];
-      if (leftJoinObj[key]) {
-        leftJoinObj[key].push(bucket.root.value[key]);
+        let key = Object.keys(currentNode.value)[0];
+        //   leftJoinObj[key] = [bucket.root.value[key]];
+        let value1 = Object.values(currentNode.value)[0];
+        let value2 = null;
+
+        if (hashTable2.contains(key)) value2 = hashTable2.get(key);
+
+        leftJoinResult.push([key, value1, value2]);
+
+        currentNode = currentNode.next;
+
       }
     }
-  });
 
-  return leftJoinObj;
+  });
+  return leftJoinResult;
 }
+
+//   hashTable2.table.forEach(bucket => {
+//     if (bucket) {
+//       let key = Object.keys(bucket.root.value)[0];
+//       if (leftJoinObj[key]) {
+//         leftJoinObj[key].push(bucket.root.value[key]);
+//       }
+//     }
+//   });
+
 
 // console.log(leftJoin(hashTable1, hashTable2));
 
 module.exports = {
-  leftJoin
+  leftJoin ,
 };
