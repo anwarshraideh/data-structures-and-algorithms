c04ae316a5db9c6dc565d1aa0c2764e0f531ede5
anwarshraideh <ahshraida@gmail.com>
2021-06-30 13:52:27 +0300

    cck

--- javascript/code-challenges/hashmap-left-join/_test_/hashmapleftjoin.test.js
+++ javascript/code-challenges/hashmap-left-join/_test_/hashmapleftjoin.test.js
@@ -1,123 +1,53 @@
 'use strict';
 
-const Hashtable = require('../../hashTable/hashTable.js');
+const HashTable = require('../../hashTable/hashTable.js');
 const {leftJoin} = require('../hashmapleftjoin.js');
 
 
-describe('leftJoin ', () => {
+describe('leftJoin 👈👈👈👈', () => {
 
-  it('should return an array with all elements in the left table joined with with right table', () => {
-    // Arrange
-    // creating hash tables and add items to it
-    const table1 = new Hashtable(1021);
-    table1.add('fond', 'enamored');
-    table1.add('wrath', 'anger');
-    table1.add('diligent', 'employed');
-    table1.add('outfit', 'garb');
-    table1.add('guide', 'usher');
+  let hashTable1;
+  let hashTable2;
+  beforeEach(() => {
+    hashTable1 = new HashTable(1024);
+    hashTable2 = new HashTable(1024);
 
-    const table2 = new Hashtable(1021);
-    table2.add('fond', 'averse');
-    table2.add('wrath', 'delight');
-    table2.add('diligent', 'idle');
-    table2.add('guide', 'follow');
-    table2.add('flow', 'jam');
-    const expectedItems = [
-      ['fond', 'enamored', 'averse'],
-      ['outfit', 'garb', null],
-      ['diligent', 'employed', 'idle'],
-      ['guide', 'usher', 'follow'],
-      ['wrath', 'anger', 'delight'],
-    ];
-    // Act
-    const result = leftJoin(table1, table2);
-    //Assert
-    expect(result.length).toBe(5);
+    hashTable1.set('fond', 'enamored');
+    hashTable1.set('wrath', 'anger');
+    hashTable1.set('diligent', 'employed');
+    hashTable1.set('outfit', 'garb');
+    hashTable1.set('guide', 'usher');
 
-    result.forEach((item, i) => {
-      expect(item.length).toBe(3);
-      expect(item).toEqual(expectedItems[i]);
-    });
+    hashTable2.set('fond', 'averse');
+    hashTable2.set('wrath', 'delight');
+    hashTable2.set('diligent', 'idle');
+    hashTable2.set('guide', 'follow');
+    hashTable2.set('flow', 'jam');
   });
 
-  it('should return an array with all elements in the first table if the second table is empty', () => {
-    // Arrange
-    // creating hash tables and add items to it
-    const table1 = new Hashtable(1021);
-    table1.add('fond', 'enamored');
-    table1.add('wrath', 'anger');
-    table1.add('diligent', 'employed');
-    table1.add('outfit', 'garb');
-    table1.add('guide', 'usher');
-
-    const table2 = new Hashtable(1021);
-    const expectedItems = [
-      ['fond', 'enamored', null],
-      ['outfit', 'garb', null],
-      ['diligent', 'employed', null],
-      ['guide', 'usher', null],
-      ['wrath', 'anger', null],
-    ];
-    // Act
-    const result = leftJoin(table1, table2);
-    //Assert
-    expect(result.length).toBe(5);
-
-    result.forEach((item, i) => {
-      expect(item.length).toBe(3);
-      expect(item).toEqual(expectedItems[i]);
-    });
+  test('🟢 works just fine 🟢', () => {
+    let expected = {
+      wrath: [ 'anger', 'delight' ],
+      outfit: [ 'garb' ],
+      diligent: [ 'employed', 'idle' ],
+      guide: [ 'usher', 'follow' ],
+      fond: [ 'enamored', 'averse' ]
+    };
+    expect(leftJoin(hashTable1, hashTable2)).toEqual(expected);
   });
 
-  it('should return empty array if the first table is empty', () => {
-    // Arrange
-    // creating hash tables and add items to it
-    const table1 = new Hashtable(1021);
-    const table2 = new Hashtable(1021);
-    table2.add('fond', 'averse');
-    table2.add('wrath', 'delight');
-    table2.add('diligent', 'idle');
-    table2.add('guide', 'follow');
-    table2.add('flow', 'jam');
-    // Act
-    const result = leftJoin(table1, table2);
-    //Assert
-    expect(result.length).toBe(0);
-    expect(result).toEqual([]);
+  test('Does not add non-corresponding key from second table', () => {
+    let result = leftJoin(hashTable1, hashTable2);
+    expect(result.flow).toBeFalsy();
   });
 
-  it('should return an array left joined if there were a collisions', () => {
-    // Arrange
-    // creating hash tables and add items to it
-    const table1 = new Hashtable(1021);
-    table1.add('fond', 'enamored');
-    table1.add('dnof', 'anger');
-    table1.add('diligent', 'employed');
-    table1.add('outfit', 'garb');
-    table1.add('tiftuo', 'usher');
+  // result looks like this
+  // {
+  //   wrath: [ 'anger', 'delight' ],
+  //   outfit: [ 'garb' ],
+  //   diligent: [ 'employed', 'idle' ],
+  //   guide: [ 'usher', 'follow' ],
+  //   fond: [ 'enamored', 'averse' ]
+  // }
 
-    const table2 = new Hashtable(1021);
-    table2.add('fond', 'averse');
-    table2.add('dnof', 'delight');
-    table2.add('diligent', 'idle');
-    table2.add('tiftuo', 'follow');
-    table2.add('flow', 'jam');
-    const expectedItems = [
-      ['fond', 'enamored', 'averse'],
-      ['dnof', 'anger', 'delight'],
-      ['outfit', 'garb', null],
-      ['tiftuo', 'usher', 'follow'],
-      ['diligent', 'employed', 'idle'],
-    ];
-    // Act
-    const result = leftJoin(table1, table2);
-    //Assert
-    expect(result.length).toBe(5);
-
-    result.forEach((item, i) => {
-      expect(item.length).toBe(3);
-      expect(item).toEqual(expectedItems[i]);
-    });
-  });
 });
-
